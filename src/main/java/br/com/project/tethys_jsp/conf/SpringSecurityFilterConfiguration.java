693cb036d5fc58bf0db24b4d54735747bf3aaf34
nagpaulo <nagpaulo@gmail.com>
2017-01-04 00:02:51 -0200

    init project

---
+++ src/main/java/br/com/project/tethys_jsp/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.project.tethys_jsp.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer
+{
+
+}
